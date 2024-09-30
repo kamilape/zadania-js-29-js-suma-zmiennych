fa020450ec672a7fa84f88bb9d072aaa38aeba40
kamilape <pelkakama@gmail.com>
2024-09-30 14:36:07 +0200

    zadania/js/29-js-suma-zmiennych

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let firstNumber = 11;
+let secondNumber = 89;
+
+
+function dodaj(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    return result;
+}
+
+
+
+console.log(dodaj(11, 89));
